d4e5b6ba38202d6f062de871c8d89698f76876fb
Saad7890-web <omysaadislam@gmail.com>
2026-02-17 18:20:43 +0600

    added worker with DB

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,1 +1,32 @@
 package worker
+
+import (
+	"log"
+	"time"
+)
+
+type Executor interface {
+	OnStart(id string)
+	OnSuccess(id string)
+	OnFailure(id string, err error)
+}
+
+type DefaultProcessor struct {
+	exec Executor
+}
+
+func NewProcessor(exec Executor) *DefaultProcessor {
+	return &DefaultProcessor{exec: exec}
+}
+
+func (p *DefaultProcessor) Process(id string, url string) {
+
+	p.exec.OnStart(id)
+
+
+	time.Sleep(2 * time.Second)
+
+	log.Println("scraped:", url)
+
+	p.exec.OnSuccess(id)
+}
